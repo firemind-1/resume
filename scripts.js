c799b2ced0210415b0770d256229ad9e964b5791
firemind-1 <lomon.egor@gmail.com>
2025-03-13 19:15:14 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,12 +4,14 @@ document.addEventListener("DOMContentLoaded", function () {
     // Попытка загрузить изображения с предсказуемыми именами
     const maxImages = 20; // Укажите максимальное количество изображений
     for (let i = 1; i <= maxImages; i++) {
-        const imgSrc = `images/toy${i}.jpg`;
+        const imgSrc = `./images/toy${i}.jpg`;
+        console.log(`Проверяем изображение: ${imgSrc}`);
         
         const img = new Image();
         img.src = imgSrc;
         img.alt = "Мягкая игрушка";
         img.onload = function () {
+            console.log(`Загружено: ${imgSrc}`);
             const imgWrapper = document.createElement("div");
             imgWrapper.classList.add("gallery-item");
             imgWrapper.appendChild(img);
