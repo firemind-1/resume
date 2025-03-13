77696d8a45f434b15cd9e587ce7c6b19f1d4eb50
firemind-1 <lomon.egor@gmail.com>
2025-03-13 19:55:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     const galleryContainer = document.getElementById("gallery");
-    
+    const lightbox = document.createElement("div");
+    lightbox.id = "lightbox";
+    document.body.appendChild(lightbox);
+
     const maxImages = 20; // Укажите максимальное количество изображений
     for (let i = 1; i <= maxImages; i++) {
         const imgSrc = `./images/toy${i}.jpg`;
@@ -13,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
         img.alt = "Мягкая игрушка";
         img.classList.add("gallery-img");
         
-        img.loading = "lazy"; // Ленивая загрузка изображений
+        img.loading = "lazy"; // Ленивая загрузка
         img.decoding = "async"; // Асинхронное декодирование
         
         img.onerror = function () {
@@ -22,5 +25,19 @@ document.addEventListener("DOMContentLoaded", function () {
         
         imgWrapper.appendChild(img);
         galleryContainer.appendChild(imgWrapper);
+        
+        img.addEventListener("click", function () {
+            lightbox.classList.add("active");
+            const lightboxImg = document.createElement("img");
+            lightboxImg.src = img.src;
+            while (lightbox.firstChild) {
+                lightbox.removeChild(lightbox.firstChild);
+            }
+            lightbox.appendChild(lightboxImg);
+        });
     }
+    
+    lightbox.addEventListener("click", function () {
+        lightbox.classList.remove("active");
+    });
 });
